f95893f2477bd586cea0ec69f896841087bde473
Stealthy-Hyrid <ggtyju@gmail.com>
2023-07-10 15:40:23 +0200

    Link vers CSS + Link Police setup

--- views/home/header.php
+++ views/home/header.php
@@ -4,7 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MSC Cahier charges</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Manrope">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="./Public/css/main.css" rel="stylesheet">
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -15,7 +17,7 @@
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item <?php if ($page == 'home') {echo 'active';}?>">
-                    <a class="nav-link" href="/MSC-1/home">Accueillette</a>
+                    <a class="nav-link" href="/MSC-1/home">Accueil</a>
                 </li>
                 <li class="nav-item <?php if ($page == 'login') {echo 'active';}?>">
                     <a class="nav-link" href="/MSC-1/login">Connexion</a>
