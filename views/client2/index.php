ff63ddc8f2260c5baebb2a05557822356dbe08ce
HatemDev <jebalihatem@gmail.com>
2023-07-10 16:34:00 +0200

    client2

--- views/client2/index.php
+++ views/client2/index.php
@@ -4,3 +4,11 @@ include_once ROOT . 'views/home/header.php';
 include_once ROOT . 'views/home/footer.php';
 ?>
 
+<h1>Suite du formualire de création de site</h1>
+<h2>Entrez les différentes informations pour un site plus personnel</h2>
+
+
+<?php
+require_once ROOT . 'App/Model.php';
+
+
