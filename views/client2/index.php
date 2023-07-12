d694ffaee1c8582fd5a589f5949ce91f271dbad3
HatemDev <jebalihatem@gmail.com>
2023-07-12 14:42:20 +0200

    form client2

--- views/client2/index.php
+++ views/client2/index.php
@@ -4,49 +4,97 @@ include_once ROOT . 'views/home/header.php';
 include_once ROOT . 'views/home/footer.php';
 ?>
 
+<div class="container text-center">
 <h1>Suite du formualire de création de site</h1>
 <h2>Entrez les différentes informations pour un site plus personnel</h2>
-
+</div>
 
 <?php
 session_start();
-require_once ROOT . 'App/Model.php';
+require_once ROOT . '/App/Model.php';
+?>
 
+<script>
+function showColorFields() {
+    var nombreCouleurs = document.getElementById('nombre-couleurs').value;
+    
+    // Affiche les champs de couleur et les labels en fonction du nombre sélectionné
+    for (var i = 1; i <= 3; i++) {
+        var couleurField = document.getElementById('couleur' + i);
+        var couleurLabel = document.getElementById('label-couleur' + i);
+        
+        if (i <= nombreCouleurs) {
+            couleurField.style.display = 'block';
+            couleurLabel.style.display = 'block';
+        } else {
+            couleurField.style.display = 'none';
+            couleurLabel.style.display = 'none';
+        }
+    }
+}
+function showLogoFields() {
+    var logoOui = document.getElementById('logo_oui');
+    var logoNon = document.getElementById('logo_non');
+    var logoFileField = document.getElementById('logo-file-field');
+    var createLogoField = document.getElementById('create-logo-field');
+    
+    // Affiche le champ de fichier si "Oui" est sélectionné, sinon affiche le champ de création de logo
+    if (logoOui.checked) {
+        logoFileField.style.display = 'block';
+        createLogoField.style.display = 'none';
+    } else if (logoNon.checked) {
+        logoFileField.style.display = 'none';
+        createLogoField.style.display = 'block';
+    }
+}
+</script>
+
+<?php
 echo "<p>Bienvenue " . $_SESSION['nom'] . " " . $_SESSION['prenom'] . " Vous avez un projet de site internet ? Renseignez vos informations nous nous occupons du reste.</p>";
 ?>
-<div class="container">
+<div class="container formulaire">
     <div class="row justify-content-center">
         <div class="col-md-6">
             <form method="post" action="">
                 <div class="form-group">
-                    <label for="pet-select">Type de site</label><br>
-                    <select name="type_site" id="type_site">
-                        <option value="">Choisissez une option</option>
-                        <option value="vitrine">Site Vitrine</option>
-                        <option value="e-commerce">E-commerce</option>
-                        <option value="portfolio">Portfolio</option>
-                        <option value="annnonce">Site d'annonce</option>
-                        <option value="association">Site pour association</option>
-                        <option value="agence-immo">Site pour agence immobilière</option>
-                    </select>
-                </div>
-                <div class="form-group">
                     <label for="nombre-couleurs">Combien de couleurs sur votre site ? (1 à 3 max)</label>
-                    <input type="number" name="nombre-couleurs" id="nombre-couleurs" min="1" max="3">
+                    <input type="number" name="nombre-couleurs" id="nombre-couleurs" min="1" max="3" oninput="showColorFields()">
                 </div>
+
+                <?php for ($i = 1; $i <= 3; $i++) { ?>
+                    <div class="form-group" id="label-couleur<?php echo $i; ?>" style="display: none;">
+                        <label for="couleur<?php echo $i; ?>">Choisissez votre couleur <?php echo ($i == 1) ? 'principale' : (($i == 2) ? 'secondaire' : 'tertiaire'); ?></label>
+                    </div>
+                    <div class="form-group">
+                        <input type="color" name="couleur<?php echo $i; ?>" id="couleur<?php echo $i; ?>" style="display: none;">
+                    </div>
+                <?php } ?>
                 <div class="form-group">
-                    <label for="couleur1">Choisissez votre couleur principale</label>
-                    <input type="color" name="couleur1" id="couleur1">
+                    <label for="logo">Avez-vous un logo ?</label>
+                    <input type="radio" name="logo" id="logo_oui" value="oui" onclick="showLogoFields()">
+                    <label for="logo_oui">Oui</label>
+                    <input type="radio" name="logo" id="logo_non" value="non" onclick="showLogoFields()">
+                    <label for="logo_non">Non</label>
                 </div>
-                <div class="form-group">
-                    <label for="couleur2">Choisissez votre couleur secondaire</label>
-                    <input type="color" name="couleur2" id="couleur2">
+
+                <div id="logo-file-field" style="display: none;">
+                    <div class="form-group">
+                        <label for="logo-file">Sélectionnez votre logo</label>
+                        <input type="file" name="logo-file" id="logo-file">
+                    </div>
                 </div>
-                <div class="form-group">
-                    <label for="couleur3">Choisissez votre couleur tertiaire</label>
-                    <input type="color" name="couleur3" id="couleur3">
+
+                <div id="create-logo-field" style="display: none;">
+                    <div class="form-group">
+                        <label for="create-logo">Souhaitez-vous que nous vous créions un logo ?</label>
+                        <input type="radio" name="create-logo" id="create-logo_oui" value="oui">
+                        <label for="create-logo_oui">Oui</label>
+                        <input type="radio" name="create-logo" id="create-logo_non" value="non">
+                        <label for="create-logo_non">Non</label>
+                    </div>
                 </div>
             </form>
         </div>
     </div>
 </div>
+
