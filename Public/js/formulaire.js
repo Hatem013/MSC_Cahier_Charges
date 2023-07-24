995002a78003bc3f3860a4336ee523e0eb8801e9
Stealthy-Hyrid <ggtyju@gmail.com>
2023-07-24 17:41:29 +0200

    JS Header

--- Public/js/formulaire.js
+++ Public/js/formulaire.js
@@ -1,20 +1,7 @@
-function showColorFields() {
-  var nombreCouleurs = document.getElementById("nombre-couleurs").value;
 
-  // Affiche les champs de couleur et les labels en fonction du nombre sélectionné
-  for (var i = 1; i <= 3; i++) {
-    var couleurField = document.getElementById("couleur" + i);
-    var couleurLabel = document.getElementById("label-couleur" + i);
 
-    if (i <= nombreCouleurs) {
-      couleurField.style.display = "block";
-      couleurLabel.style.display = "block";
-    } else {
-      couleurField.style.display = "none";
-      couleurLabel.style.display = "none";
-    }
-  }
-}
+// Selection oui ou non du logo
+
 function showLogoFields() {
   var logoOui = document.getElementById("logo_oui");
   var logoNon = document.getElementById("logo_non");
@@ -23,9 +10,9 @@ function showLogoFields() {
   var logo_preview = document.getElementById("logo_file_preview");
   var logo_file = document.getElementById("logo_file");
 
-  // Affiche la preview si "Oui" est sélectionné, sinon affiche le message 
-  
-    if (logoOui.checked) {
+  // Affiche la preview si "Oui" est sélectionné, sinon affiche le message
+
+  if (logoOui.checked) {
     logoFileField.style.display = "block";
     createLogoField.style.display = "none";
 
@@ -59,35 +46,4 @@ function testing() {
 }
 */
 
-
-// Client3
-var header_d = document.getElementById("header_desktop_btn");
-var header_d_c = document.getElementById("header_desktop_card");
-var header_m_c = document.getElementById("header_mobile_card");
-
-var header_collapse = document.querySelectorAll(".collapse")[1];
-var header_mobile_collapse = document.querySelectorAll(".collapse")[2];
-
-
-var header_desktop = document.querySelectorAll(".header_desktop")
-
-for (var i = 0; i<header_desktop.length; i++) {
-
-  header_desktop[i].addEventListener("click", colorUpdate);
-}
-
-function colorUpdate() {
-
-header_d.classList.add("btn_validate");
-
-var collapse = new bootstrap.Collapse(header_collapse);
-var collapsing = new bootstrap.Collapse(header_mobile_collapse);
-
-
-}
-
-console.log(header_desktop)
-
-
-
-
+////// Client3 header
